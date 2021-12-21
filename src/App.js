0a998a00b0a4d5e895361358907b9de080280336
Johan_David <jhoanpor78@gmail.com>
2021-12-21 10:47:05 -0500

    Facturacion y Fondo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import './Styles/App.css';
 import Facturacion from './Components/Facturacion'
-import Home from './Components/Home'
 
 function App() {
   return <div className="App">
-      <Home/>
+      <Facturacion/>
     </div>
 }
 
